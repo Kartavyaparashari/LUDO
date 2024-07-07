a4c8a55e913539909c380ea6525b40309b870306
Kartavyaparashari <162548290+Kartavyaparashari@users.noreply.github.com>
2024-07-08 01:01:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
     audio.loop = true; 
     const button = document.getElementById('audioButton');
     button.click();
-    
     button.addEventListener('click', () => {
         if (audio.paused) {
             audio.play();
@@ -12,7 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
             audio.pause();
         }
     });
-  
 });
 const chBtnLength=document.querySelectorAll(".ch-btn").length;
 for(var i=0 ; i<chBtnLength;i++){
@@ -22,10 +20,13 @@ document.querySelectorAll(".ch-btn")[i].addEventListener("click" , function (){
    document.querySelector(".contnr").classList.toggle("d-none");
    setupBoard();
 });
+ var btnAudio = document.getElementById('btnAudio');
+ document.querySelectorAll(".ch-btn")[i].addEventListener('focus', function() {
+        btnAudio.currentTime = 0; 
+        btnAudio.play();
+      });
 }
-
 // document.getElementById('btn').addEventListener('click', setupBoard);
-
 const colors = ['red', 'blue', 'green', 'yellow'];
 function setupBoard() {
     const board = document.getElementById('gameBoard');
@@ -78,3 +79,39 @@ function setupBoard() {
         board.appendChild(cell);
     }
 }
+const dice=document.querySelectorAll(".dice-img").length;
+for(let n=0; n<dice; n++){
+document.querySelectorAll(".dice-cont")[n].addEventListener("click" , function (){
+    const rn1=Math.floor(Math.random() * 6) + 1;
+    const rn2=Math.floor(Math.random() * 6) + 1; 
+    const rn3=Math.floor(Math.random() * 6) + 1;  
+    const rn4=Math.floor(Math.random() * 6) + 1; 
+    if(n===0){
+        document.getElementById("dice-img1").setAttribute("src" , "./images/dice"+rn1+".png");
+    }
+    else if(n===1){
+        document.getElementById("dice-img2").setAttribute("src" ,"./images/dice"+rn2+".png");
+    }
+    else if(n===2){
+        document.getElementById("dice-img3").setAttribute("src" , "./images/dice"+rn3+".png");
+    }
+    else if(n===3){
+        document.getElementById("dice-img4").setAttribute("src" ,"./images/dice"+rn4+".png");
+    }
+    else{
+        alert("error "+n);
+    }
+    diceAudio.currentTime = 0; 
+    diceAudio.play();
+//     var diceAudio = document.getElementById('diceAudio');
+//  document.querySelectorAll(".dice-img")[n].addEventListener('focus', function() {
+    
+//       });
+});
+}
+
+  
+    // document.getElementById("img1").setAttribute("src" , "./images/dice"+rn1+".png");
+    // document.getElementById("img2").setAttribute("src" ,"./images/dice"+rn2+".png");
+    // document.getElementById("img3").setAttribute("src" , "./images/dice"+rn1+".png");
+    // document.getElementById("img4").setAttribute("src" ,"./images/dice"+rn2+".png");
